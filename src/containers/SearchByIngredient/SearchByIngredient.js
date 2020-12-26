52db458c0b261649d904a360620f3011bd53e572
uycuhnt2467 <uycuhnt2467@gmail.com>
2020-12-26 09:30:40 +0800

    Add Search Ingredient page

--- src/containers/SearchByIngredient/SearchByIngredient.js
+++ src/containers/SearchByIngredient/SearchByIngredient.js
@@ -16,43 +16,47 @@ class SearchIngredient extends Component {
     //     this.state = {...}
     // }
     state = {
-        // ingredient_list: [""],
-        matching_recipe: [
+        allIngredients: [
             // {
-            //     recipe_id: "",
-            //     recipe_name: "",
+            //     ingredient_id: "",
+            //     ingredient_name: "",
+            //     img_url: "",
+            // },
+        ],
+        currentIngredients: [
+            // {
+            //     ingredient_id: "",
+            //     ingredient_name: "",
             //     img_url: "",
             // },
         ],
         currentQuery: "",
+        loading: true,
         success: false,
-        loading: false,
+        // err: "",
     };
 
     componentDidMount() {
         // const cors = "https://cors-anywhere.herokuapp.com/";
-        const url =
-            "http://3.12.253.9:3000/search";
-        axios
-            .post(url, { ingredients: this.props.ingredient_list })
-            .then((res) => {
-                // http://18.222.31.30/leftover_killer/get_recipes.php
-                // http://localhost/leftoverkiller2/get_recipes.php
-                // console.log(res.data);
-                const recipes = res.data.result.recipes;
-                // console.log(recipes);
-                if (res.data.result.success) {
-                    this.setState({
-                        success: res.data.result.success,
-                        matching_recipe: recipes,
-                        loading: true,
-                    });
-                } else {
-                    console.log("no return");
-                }
-
-                //
-            });
+        const url = "http://3.12.253.9:3000/ingredient";
+        axios.get(url).then((res) => {
+            if (res.data.result.success) {
+                this.setState({
+                    success: true,
+                    allIngredients: res.data.result.ingredients,
+                    currentIngredients: res.data.result.ingredients,
+                    loading: false,
+                });
+            } else {
+                this.setState({
+                    success: false,
+                    allIngredients: [],
+                    currentIngredients: [],
+                    loading: false,
+                });
+                console.log("no return");
+            }
+        });
     }
 
     handleIngredientInputChange = (e) => {
@@ -65,55 +69,55 @@ class SearchIngredient extends Component {
         });
     };
 
-    handleAddClick = (e) => {
-        e.preventDefault();
-        this.props.onIngredientAdded(this.state.currentQuery);
-        this.setState((prevState) => ({
-            currentQuery: "",
-        }));
-    };
-
-    handleRemoveClick = (e) => {
-        e.preventDefault();
-        this.props.onIngredientRemoved(e.target.value);
-    };
-
     handleSearchClick = (e) => {
         e.preventDefault();
-        const cors = "https://cors-anywhere.herokuapp.com/";
-        const url =
-            "http://18.222.31.30/leftover_killer/get_matching_recipes.php";
-        axios
-            .post(`${cors}${url}`, { ingredients: this.props.ingredient_list })
-            .then((res) => {
-                // http://18.222.31.30/leftover_killer/get_recipes.php
-                // http://localhost/leftoverkiller2/get_recipes.php
-                // console.log(res.data);
-                const recipes = res.data.recipes;
-                // console.log(recipes);
-                if (res.data.success) {
-                    this.setState({
-                        success: res.data.success,
-                        matching_recipe: recipes,
-                    });
-                } else {
-                    console.log("no return");
-                }
 
-                //
-            });
+        let newIngredientsResult = this.state.allIngredients.filter((ing) => {
+            return ing.ingredient_name
+                .toLowerCase()
+                .includes(this.state.currentQuery);
+        });
+
+        this.setState((prevState) => {
+            return {
+                ...prevState,
+                currentIngredients: newIngredientsResult,
+            };
+        });
     };
 
     render() {
-        let cur_ingredient = this.props.ingredient_list.map((val, idx) => {
-            return ingredient_display(idx, val, this.handleRemoveClick);
-        });
-
-        let found_recipe = this.state.matching_recipe.map((val, idx) => {
-            return recipe_display(idx, val);
-        });
+        let curIngredient = <Spinner />;
+        if (!this.state.loading) {
+            curIngredient = this.state.currentIngredients.map((val, idx) => {
+                return ingredient_info(idx, val);
+            });
+        }
+         
 
         return (
+            // <Aux>
+            //     <input
+            //         type="text"
+            //         onChange={this.handleIngredientInputChange}
+            //         name="curSearch"
+            //         placeholder="Any Ingredient"
+            //         value={this.state.currentQuery}
+            //     ></input>
+            //     <button type="submit" onClick={this.handleAddClick}>
+            //         Add
+            //     </button>
+            //     <button type="submit" onClick={this.handleSearchClick}>
+            //         Search
+            //     </button>
+
+            //     <h1>Current Ingredient</h1>
+            //     {cur_ingredient ? cur_ingredient : null}
+
+            //     <h1>Found Recipe</h1>
+
+            //     {found_recipe.length > 0 ? found_recipe : null}
+            // </Aux>
             <Aux>
                 <input
                     type="text"
@@ -122,48 +126,27 @@ class SearchIngredient extends Component {
                     placeholder="Any Ingredient"
                     value={this.state.currentQuery}
                 ></input>
-                <button type="submit" onClick={this.handleAddClick}>
-                    Add
-                </button>
                 <button type="submit" onClick={this.handleSearchClick}>
                     Search
                 </button>
-
-                <h1>Current Ingredient</h1>
-                {cur_ingredient ? cur_ingredient : null}
-
-                <h1>Found Recipe</h1>
-
-                {found_recipe.length > 0 ? found_recipe : null}
+                {curIngredient}
             </Aux>
         );
     }
 }
-
-const ingredient_display = (key, ingredient_value, fun) => {
+// define components
+function ingredient_info(key, ingredient_data) {
+    let link_string = "/ingredient/" + ingredient_data.ingredient_id;
     return (
         <ul key={key}>
-            <li>{ingredient_value}</li>
+            <li>{ingredient_data.ingredient_id}</li>
+            <li>{ingredient_data.ingredient_name}</li>
             <li>
-                <button type="submit" onClick={fun} value={ingredient_value}>
-                    Remove
-                </button>
+                <Link to={link_string}>ingredient link</Link>
             </li>
         </ul>
     );
-};
-
-const recipe_display = (key, recipe) => {
-    let link_string = "/recipe/" + recipe.recipe_id;
-    return (
-        <ul key={key}>
-            <li>{recipe.recipe_name}</li>
-            <li>
-                <Link to={link_string}>recipe link</Link>
-            </li>
-        </ul>
-    );
-};
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -171,17 +154,17 @@ const mapStateToProps = (state) => {
     };
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onIngredientAdded: (ingName) =>
-            dispatch(actions.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) =>
-            dispatch(actions.removeIngredient(ingName)),
-    };
-};
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         onIngredientAdded: (ingName) =>
+//             dispatch(actions.addIngredient(ingName)),
+//         onIngredientRemoved: (ingName) =>
+//             dispatch(actions.removeIngredient(ingName)),
+//     };
+// };
 
 // export default connect(
 //     mapStateToProps,
 //     mapDispatchToProps
 // )(withErrorHandler(BurgerBuilder, axios));
-export default connect(mapStateToProps, mapDispatchToProps)(SearchIngredient);
+export default connect(mapStateToProps, null)(SearchIngredient);
