1101dc4cfc099644fa5adb3e972fb7288304b30e
uycuhnt2467 <uycuhnt2467@gmail.com>
2020-12-29 14:19:24 +0800

    Add UI: Ingredient Detail page

--- src/containers/Ingredient/IngredientDetail/IngredientDetail.js
+++ src/containers/Ingredient/IngredientDetail/IngredientDetail.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import classes from "./IngredientDetail.module.css";
 
 const ingredientDetailInfo = (props) => {
@@ -33,6 +35,12 @@ const ingredientDetailInfo = (props) => {
 
     return (
         <div className={classes.item} key={props.ingredientInfo.ingredient_id}>
+            <div className={classes.itemName}>
+                {props.ingredientInfo.ingredient_name
+                    .split(" ")
+                    .map(capitalize)
+                    .join(" ")}
+            </div>
             <div className={classes.imgSize}>
                 <img
                     src={props.ingredientInfo.image_url}
@@ -40,21 +48,32 @@ const ingredientDetailInfo = (props) => {
                     alt="lala"
                 />
             </div>
-            <div className={classes.itemName}>
-                {props.ingredientInfo.ingredient_name
-                    .split(" ")
-                    .map(capitalize)
-                    .join(" ")}
-            </div>
-            <div className={classes.itemButtonDiv}>{buttonIng}</div>
 
-            <div>{recipe_infos}</div>
+            <div className={classes.itemButtonDiv}>{buttonIng}</div>
+            <div className={classes.topRecipeTitle}>Top Recipe</div>
+            <div className={classes.displayRecipe}>{recipe_infos}</div>
         </div>
     );
 };
 
 function recipe_info(key, recipe) {
-    return <div key={key}>{recipe.recipe_name}</div>;
+    let link_string = "/recipe/" + recipe.recipe_id;
+    return (
+        <div className={classes.displayItem} key={recipe.recipe_id}>
+            <div className={classes.displayItemImgSize}>
+                <Link to={link_string}>
+                    <img
+                        className={classes.displayItemImg}
+                        src={recipe.img_url}
+                        alt="lalala"
+                    />
+                </Link>
+            </div>
+            <div>
+                <p className={classes.displayItemP}>{recipe.recipe_name}</p>
+            </div>
+        </div>
+    );
 }
 
 export default ingredientDetailInfo;
