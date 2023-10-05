2b97afcdad3cf50144263c7e14fc40c7008cb317
uycuhnt2467 <uycuhnt2467@gmail.com>
2023-10-04 21:55:35 -0400

    modify: turn pantry page into hook components

--- src/containers/Pantry/Pantry.js
+++ src/containers/Pantry/Pantry.js
@@ -1,105 +1,80 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
-import { Link, withRouter, Redirect} from "react-router-dom";
+import { Link, withRouter, Redirect } from "react-router-dom";
 
 import Aux from "../../hoc/Auxx/Auxx";
-
 import Spinner from "../../components/UI/Spinner/Spinner";
-// import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import PantryIngredientDisplay from "./PantryIngredientDisplay/PantryIngredientDisplay";
 import classes from "./Pantry.module.css";
 import * as actions from "../../store/actions/index";
 
-class Pantry extends Component {
-    state = {
-        redirect:false,
-        // ingredient_list: [""],
-        ingredients: [
-            // {
-            //     recipe_id: "",
-            //     recipe_name: "",
-            //     img_url: "",
-            // },
-        ],
-        currentQuery: "",
-        success: false,
-    };
+const Pantry = (props) => {
+    const [currentQuery, setCurrentQuery] = useState("");
+    const [redirect, setRedirect] = useState(false);
 
-    componentDidMount() {
-        // const cors = "https://cors-anywhere.herokuapp.com/";
-        // console.log(this.props.token_id);
-        this.props.onInitializePantry(this.props.token_id);
-        // console.log("initilaize favorite");
-        // console.log(this.props.ingredient_list);
-    }
+    useEffect(() => {
+        props.onInitializePantry(props.token_id);
+    }, [props.token_id]);
 
-    handleIngredientInputChange = (e) => {
+    const handleIngredientInputChange = (e) => {
         e.preventDefault();
-        this.setState((prevState) => {
-            return {
-                ...prevState,
-                currentQuery: e.target.value,
-            };
-        });
+        setCurrentQuery(e.target.value);
     };
 
-    redirectHandler = () => {
-        this.setState({ redirect: true })
-        this.renderRedirect();
-    }
-    renderRedirect = () => {
-        if (this.state.redirect) {
-            return <Redirect to='/matching' />
-        }
-    }
+    const redirectHandler = () => {
+        setRedirect(true);
+    };
 
-    handleSearchClick = (e) => {
+    const renderRedirect = () => {
+        if (redirect) {
+            return <Redirect to='/matching' />;
+        }
+    };
+
+    const handleSearchClick = (e) => {
         e.preventDefault();
     };
 
-    render() {
-        let cur_ingredient = <Spinner />;
+    let cur_ingredient = <Spinner />;
 
-        if (!this.props.loading) {
-            if (this.props.ingredient_list.length === 0) {
-                cur_ingredient = (
-                    <h1 className={classes.noItemTitle}>
-                        No ingredient in the pantry, add ingredient{" "}
-                        <Link to="/searchbyingredient">here</Link>.
-                    </h1>
-                );
-            } else {
-                cur_ingredient = this.props.ingredient_list.map((val) => {
-                    return PantryIngredientDisplay(val);
-                });
-            }
+    if (!props.loading) {
+        if (props.ingredient_list.length === 0) {
+            cur_ingredient = (
+                <h1 className={classes.noItemTitle}>
+                    No ingredient in the pantry, add ingredient{" "}
+                    <Link to="/searchbyingredient">here</Link>.
+                </h1>
+            );
+        } else {
+            cur_ingredient = props.ingredient_list.map((val) => {
+                return PantryIngredientDisplay(val);
+            });
         }
-
-        return (
-            <Aux>
-                <div className={classes.inputDiv}>
-                    <h1>Ingredient in Pantry</h1>
-                    <input
-                        type="text"
-                        onChange={this.handleIngredientInputChange}
-                        name="curSearch"
-                        placeholder="Any Ingredient"
-                        value={this.state.currentQuery}
-                    ></input>
-                    <button type="submit" onClick={this.handleSearchClick}>
-                        Search
-                    </button>
-                    <button type="submit" onClick={this.redirectHandler}>
-                        Matching
-                    </button>
-                    {this.renderRedirect()}
-                </div>
-
-                <div className={classes.flex_container}>{cur_ingredient}</div>
-            </Aux>
-        );
     }
-}
+
+    return (
+        <Aux>
+            <div className={classes.inputDiv}>
+                <h1>Ingredient in Pantry</h1>
+                <input
+                    type="text"
+                    onChange={handleIngredientInputChange}
+                    name="curSearch"
+                    placeholder="Any Ingredient"
+                    value={currentQuery}
+                />
+                <button type="submit" onClick={handleSearchClick}>
+                    Search
+                </button>
+                <button type="submit" onClick={redirectHandler}>
+                    Matching
+                </button>
+                {renderRedirect()}
+            </div>
+            <div className={classes.flex_container}>{cur_ingredient}</div>
+        </Aux>
+    );
+};
 
 const mapStateToProps = (state) => {
     return {
@@ -111,17 +86,9 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onIngredientAdded: (ingName) =>
-            dispatch(actions.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) =>
-            dispatch(actions.removeIngredient(ingName)),
         onInitializePantry: (token_id) =>
             dispatch(actions.authPantry(token_id)),
     };
 };
 
-// export default connect(
-//     mapStateToProps,
-//     mapDispatchToProps
-// )(withErrorHandler(BurgerBuilder, axios));
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Pantry));
